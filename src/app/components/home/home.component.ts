149b8382aef99129bfe8110d4c754f57dca58a0b
nasii05 <iamnasih@gmail.com>
2023-03-08 14:35:34 +0530

    few new changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,29 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-   product = [
-     {
-      img:"../../../assets/images/women/girl1.jpg",
-      heading:"Lehenga"
-     },
-     {
-      img:"../../../assets/images/women/women1.webp",
-      heading:"party wears"
-     },
-     {
-      img:"../../../assets/images/women/men2.jpg",
-      heading:"casuals"
-     },
-     {
-      img:"../../../assets/images/women/women2.jpg",
-      heading:"elegant"
-     },
-     {
-      img:"../../../assets/images/women/men1.jpg",
-      heading:"men's formals"
-     }
-   ]
+  product = [
+    {
+      img: '../../../assets/images/women/girl1.jpg',
+      heading: 'Lehenga',
+    },
+    {
+      img: '../../../assets/images/women/women1.webp',
+      heading: 'party wears',
+    },
+    {
+      img: '../../../assets/images/women/men2.jpg',
+      heading: 'casuals',
+    },
+    {
+      img: '../../../assets/images/women/women2.jpg',
+      heading: 'elegant',
+    },
+    {
+      img: '../../../assets/images/women/men1.jpg',
+      heading: "men's formals",
+    },
+  ];
 }
+
